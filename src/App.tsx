691b8fab432b973339a5cbe8f126827eef06deb4
Mikołaj Tęcza <mikolaj.tecza@rumblefish.pl>
2021-12-03 12:02:12 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC, useState } from "react";
+import Header from "./components/Header/Header";
+import Exchanger from "./components/Exchanger/Exchanger";
+import { Web3Provider } from "@ethersproject/providers";
+import { Web3ReactProvider } from "@web3-react/core";
+import { UserBalanceProvider } from "./context/UserBalance";
+import Updater from "./components/Updater/Updater";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { defaultStyles, darkStyles } from "./styles/themes";
+import { ToggleTheme } from "./styles/ToggleTheme";
 
-function App() {
+const App: FC = () => {
+  const getLibrary = (provider: any) => {
+    const library = new Web3Provider(provider);
+    library.pollingInterval = 12000;
+    return library;
+  };
+
+  const [isDark, setIsDark] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <UserBalanceProvider>
+        <ThemeProvider theme={isDark ? darkStyles : defaultStyles}>
+          <GlobalStyle isDark={isDark} />
+          <Updater />
+          <Header>
+            <span>Dark theme </span>
+            <ToggleTheme onClick={() => setIsDark(!isDark)} isDark={isDark}>
+              <div></div>
+            </ToggleTheme>
+          </Header>
+          <main>
+            <Exchanger />
+          </main>
+        </ThemeProvider>
+      </UserBalanceProvider>
+    </Web3ReactProvider>
   );
-}
+};
 
 export default App;
