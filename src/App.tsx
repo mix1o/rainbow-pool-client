c1e6ea19f5faeac09dba6f40ac4dcb8d90cde478
Mikołaj Tęcza <mikolaj.tecza@rumblefish.pl>
2021-12-03 15:12:51 +0100

    Removed function getLibrary from lifecycle of component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import { FC, useState } from "react";
 import Header from "./components/Header/Header";
 import Exchanger from "./components/Exchanger/Exchanger";
-import { Web3Provider } from "@ethersproject/providers";
 import { Web3ReactProvider } from "@web3-react/core";
 import { UserBalanceProvider } from "./context/UserBalance";
 import Updater from "./components/Updater/Updater";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import { defaultStyles, darkStyles } from "./styles/themes";
-import { ToggleTheme } from "./styles/ToggleTheme";
+import { Circle, ToggleTheme } from "./styles/ToggleTheme";
+import { getLibrary } from "./functions/getLibrary";
 
 const App: FC = () => {
-  const getLibrary = (provider: any) => {
-    const library = new Web3Provider(provider);
-    library.pollingInterval = 12000;
-    return library;
-  };
-
   const [isDark, setIsDark] = useState(false);
 
   return (
@@ -28,7 +22,7 @@ const App: FC = () => {
           <Header>
             <span>Dark theme </span>
             <ToggleTheme onClick={() => setIsDark(!isDark)} isDark={isDark}>
-              <div></div>
+              <Circle isDark={isDark} />
             </ToggleTheme>
           </Header>
           <main>
